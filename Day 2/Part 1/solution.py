341865fe40c925e97333814e91704663ea5a1fac
LozonR <lozonr@uwindsor.ca>
2025-12-02 01:42:42 -0500

    Day 2

---
+++ Day 2/Part 1/solution.py
@@ -0,0 +1,20 @@
+from sys import argv
+import re
+
+
+def solution():
+    pattern = r"^(?P<group>\d{1,100})(?P=group)$"
+
+    sum = 0
+    with open(argv[1], "r") as file:
+        contents = file.readline().strip()
+        for interval in contents.split(","):
+            for i in range(int(interval.split("-")[0]), int(interval.split("-")[1]) + 1):
+                mat = re.match(pattern, str(i))
+                if mat:
+                    sum += int(i)
+    print(sum)
+
+if __name__ == "__main__":
+    solution()
+    
